8e5a0d7877c15b10b333ebf7aff57787dc8550cc
bitwiserob <kaczurr@gmail.com>
2024-04-05 19:40:52 -0400

    added numpy pandas

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import render_template,request
 from flask_bootstrap import Bootstrap
+import plotly.graph_objs as go
+import plotly
+import json
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -14,14 +17,16 @@ def index():
 def predict():
     form_data = {}
     if request.method == 'POST':
-        print(request.form)
         # Assuming you're sending data with the keys 'air_temp', 'rpm', 'nm', 'min'
         form_data['air_temp'] = request.form.get('air_temp', '')
         form_data['rpm'] = request.form.get('rpm', '')
         form_data['nm'] = request.form.get('nm', '')
         form_data['min'] = request.form.get('min', '')
-    return render_template('predict.html', form_data=form_data)
+    data = [go.Bar(x=[1, 2, 3], y=[4, 5, 6])]
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('predict.html', form_data=form_data,graphJSON=graphJSON)
 
+    
 if __name__ == '__main__':
     app.run(debug=True)
 #load model on startup
