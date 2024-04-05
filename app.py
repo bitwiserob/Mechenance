173f8fc7133c21bad97f5879e369119c38834810
bitwiserob <kaczurr@gmail.com>
2024-04-05 19:16:20 -0400

    post values work

--- app.py
+++ app.py
@@ -14,11 +14,12 @@ def index():
 def predict():
     form_data = {}
     if request.method == 'POST':
+        print(request.form)
         # Assuming you're sending data with the keys 'air_temp', 'rpm', 'nm', 'min'
-        form_data['air_temp'] = request.form.get('air_temp_input', '')
-        form_data['rpm'] = request.form.get('rpm_input', '')
-        form_data['nm'] = request.form.get('nm_input', '')
-        form_data['min'] = request.form.get('min_input', '')
+        form_data['air_temp'] = request.form.get('air_temp', '')
+        form_data['rpm'] = request.form.get('rpm', '')
+        form_data['nm'] = request.form.get('nm', '')
+        form_data['min'] = request.form.get('min', '')
     return render_template('predict.html', form_data=form_data)
 
 if __name__ == '__main__':
